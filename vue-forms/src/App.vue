d6903cc0518f45b296b1b5eaffc51208cd2ac76a
Leonardo Giraldi <leogiraldimg@gmail.com>
2021-07-12 07:09:55 -0300

    feat: includes first form

--- vue-forms/src/App.vue
+++ vue-forms/src/App.vue
@@ -1,5 +1,65 @@
 <template>
   <div>
     <h3>Payment</h3>
+    <form novalidate>
+      <div class="row">
+        <div class="col-md-6">
+          <div><strong>Shipping Information</strong></div>
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input id="name" type="text" class="form-control" placeholder="Your Name" />
+          </div>
+          <div class="form-group">
+            <label for="company">Company Name</label>
+            <input id="company" type="text" class="form-control" placeholder="Company" />
+          </div>
+          <div class="form-group">
+            <label for="address1">Address</label>
+            <input id="address1" type="text" class="form-control" placeholder="Street Address" />
+          </div>
+          <div class="form-group">
+            <label for="address2">Suite/Apartment #</label>
+            <input id="address2" type="text" class="form-control" placeholder="" />
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="cityTown">City</label>
+              <input
+                id="cityTown"
+                type="text"
+                class="form-control"
+                placeholder="e.g. New York" 
+              />
+            </div>
+            <div class="form-group col-md-3">
+              <label for="stateProvince">State</label>
+              <select
+                id="stateProvince"
+                class="form-control"
+              >
+                <option>GA</option>
+                <option>CA</option>
+                <option>FL</option>
+                <option>AL</option>
+              </select>
+            </div>
+            <div class="form-group col-md-3">
+              <label for="postalCode">Zipcode</label>
+              <input
+                id="postalCode"
+                type="text"
+                class="form-control"
+                placeholder="e.g. 10101">
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Next" class="btn btn-success" />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div><strong>Billing Information</strong></div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
